a89553d387988c42c05a04173fcbfbe5ac23a174
hnikoloski <h.nikoloski@outlook.com>
2023-04-25 01:55:59 +0200

    preload google fonts

--- header.php
+++ header.php
@@ -15,6 +15,8 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
